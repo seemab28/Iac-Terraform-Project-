497e4e8b76506db724d2ab2a2e67da9a8c3f2de0
seemab28 <naavadagiseema@gmail.com>
2024-11-07 13:38:15 +0530

    Add files via upload

---
+++ v1-create_before_destroy/c2-ec2-instance.tf
@@ -0,0 +1,19 @@
+# Create EC2 Instance
+resource "aws_instance" "web" {
+  ami               = "ami-0ebfd941bbafe70c6" # Amazon Linux
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  #availability_zone = "us-east-1b"
+  tags = {
+    "Name" = "web-4"
+  }
+ 
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      tags,
+    ]
+  }
+
+}
